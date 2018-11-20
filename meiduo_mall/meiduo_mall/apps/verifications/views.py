d8c7af72f5478ed6749389082c7bff016a156bd1
qinyucun <1060309059@qq.com>
2018-11-20 19:59:14 +0800

    创建手机验证码端口定义完毕

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class SMSCodeView(APIView):
+    def get(self, request, mobile):
+        """
+
+        :param request:
+        :param mobile:
+        :return:
+        """
+        return Response({"message": "ok"})
